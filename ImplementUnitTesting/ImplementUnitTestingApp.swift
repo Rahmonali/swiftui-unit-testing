b4d3de505efe623e1c15fe7546efa731136eaa72
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-07-10 16:59:11 +0500

    Initial Commit

---
+++ ImplementUnitTesting/ImplementUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplementUnitTestingApp.swift
+//  ImplementUnitTesting
+//
+//  Created by Rahmonali on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ImplementUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
